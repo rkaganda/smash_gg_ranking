a9218966f9f9d08f9b7fc2649909b377c196b7c4
rkaganda <rkaganda@gmail.com>
2022-04-03 11:52:15 -0700

    added paging for matches

--- smash_gg_ranking/app.py
+++ smash_gg_ranking/app.py
@@ -51,19 +51,23 @@ def view_ranking_participants(ranking_id=None):
 
 @app.route('/ranking/<ranking_id>/event/<event_id>/matches')
 def view_ranking_event_matches(ranking_id=None, event_id=None):
-    ranking_data, event_data, event_sets = matches.get_ranking_sets(ranking_id, event_id)
-    return render_template('ranking/event/matches.html', event_sets=event_sets, ranking_data=ranking_data,
-                           event_data=event_data)
+    paging = generate_paging()
+    ranking_data, event_data, event_sets, paging_info = matches.get_ranking_sets(ranking_id, event_id, paging)
+    return render_template('ranking/event/matches.html',
+                           event_sets=event_sets, ranking_data=ranking_data,
+                           event_data=event_data, paging_info=paging_info)
 
 
 @app.route('/ranking/<ranking_id>/participant/<participant_id>/matches')
 @app.route('/ranking/<ranking_id>/participant/<participant_id>/matches/')
 def view_ranking_participant_matches(ranking_id=None, participant_id=None):
+    paging = generate_paging()
     event_id = request.args.get('event', default=None, type=int)
-    ranking_data, event_data, participant_data, participant_sets = matches.get_participant_sets(ranking_id, event_id,
-                                                                                                participant_id)
-    return render_template('ranking/participant/matches.html', participant_sets=participant_sets,
-                           participant_data=participant_data, ranking_data=ranking_data, event_data=event_data)
+    ranking_data, event_data, participant_data, participant_sets, paging_info = matches.get_participant_sets(
+        ranking_id, event_id, participant_id, paging)
+    return render_template('ranking/participant/matches.html',
+                           participant_sets=participant_sets, participant_data=participant_data,
+                           ranking_data=ranking_data, event_data=event_data, paging_info=paging_info)
 
 
 # @app.route('/ranking/<ranking_id>/event/<event_id>/participant/<participant_id>/matches')
--- smash_gg_ranking/views/matches.py
+++ smash_gg_ranking/views/matches.py
@@ -1,5 +1,6 @@
 import logging
 from sqlalchemy import and_, or_
+from typing import Dict
 
 from db import db
 from db.models import Ranking, RankingSet, RankingEvent, Participant
@@ -13,17 +14,33 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 
 
-def get_ranking_sets(ranking_id: int, event_id: int):
+def get_paging_info(sets, paging: Dict):
+    # paging
+    participant_sets_count = sets.count()
+    paging_info = {
+        "page_num": paging['page_num'],
+        "max_page": (participant_sets_count // paging['page_size']) + 1,  # math is hard?
+        "page_size": paging['page_size']
+    }
+    offset = (paging['page_num'] - 1) * paging['page_size']
+    participant_sets = sets.offset(offset).limit(paging['page_size'])
+
+    return participant_sets, paging_info
+
+
+def get_ranking_sets(ranking_id: int, event_id: int, paging: Dict):
     sets = []
     session = db.get_session()
     with session() as session:
         ranking = session.query(Ranking).where(Ranking.id == ranking_id).first()
 
         ranking_sets = session.query(RankingSet).where(
-            RankingSet.ranking_event_id == event_id).order_by(RankingSet.set_datetime.desc()).all()
+            RankingSet.ranking_event_id == event_id).order_by(RankingSet.set_datetime.desc())
         event = session.query(RankingEvent).where(RankingEvent.id == event_id).first()
         event = event.__dict__
         event['attrib'] = graph_query.get_event_attributes(graph_query.parse_event_url(event['event_url']))
+
+        ranking_sets, paging_info = get_paging_info(ranking_sets, paging)
 
         # TODO refactor to mapping
         participants = session.query(Participant).all()
@@ -31,7 +48,7 @@ def get_ranking_sets(ranking_id: int, event_id: int):
         for par in participants:
             par_tags[par.id] = par.gamer_tag
 
-        for rs in ranking_sets:
+        for rs in ranking_sets.all():
             sets.append({
                 "winner_id": rs.winner_id,
                 "winner_gamertag": par_tags[rs.winner_id],
@@ -46,51 +63,10 @@ def get_ranking_sets(ranking_id: int, event_id: int):
                 "set_datetime": rs.set_datetime,
             })
 
-    return ranking.__dict__, event, sets
+    return ranking.__dict__, event, sets, paging_info
 
 
-# def get_participant_sets(ranking_id: int, participant_id: str):
-#     logging.debug("participant_id={}".format(participant_id))
-#     sets = []
-#     session = db.get_session()
-#     with session() as session:
-#         ranking = session.query(Ranking).where(Ranking.id == ranking_id).first()
-#
-#         participant_sets = session.query(RankingSet).where(
-#             and_(or_(RankingSet.loser_id == participant_id, RankingSet.winner_id == participant_id),
-#                  RankingSet.ranking_id == ranking.id
-#                  )
-#         ).order_by(RankingSet.set_datetime.desc())
-#
-#         # TODO refactor to mapping
-#         participants = session.query(Participant).all()
-#         par_tags = {}
-#         for par in participants:
-#             par_tags[par.id] = par.gamer_tag
-#         participant_data = {
-#             "id": participant_id,
-#             "gamertag": par_tags[participant_id]
-#         }
-#
-#         for ps in participant_sets:
-#             sets.append({
-#                 "winner_id": ps.winner_id,
-#                 "winner_gamertag": par_tags[ps.winner_id],
-#                 "winner_score": ps.winner_score,
-#                 "winner_points": ps.winner_points,
-#                 "winner_change": ps.winner_change,
-#                 "loser_id": ps.loser_id,
-#                 "loser_gamertag": par_tags[ps.loser_id],
-#                 "loser_score": ps.loser_score,
-#                 "loser_points": ps.loser_points,
-#                 "loser_change": ps.loser_change,
-#                 "set_datetime": ps.set_datetime,
-#             })
-#
-#     return ranking.__dict__, participant_data, sets
-
-
-def get_participant_sets(ranking_id: int, event_id: int, participant_id: str):
+def get_participant_sets(ranking_id: int, event_id: int, participant_id: str, paging: Dict):
     logging.debug("participant_id={}".format(participant_id))
     sets = []
     session = db.get_session()
@@ -115,6 +91,8 @@ def get_participant_sets(ranking_id: int, event_id: int, participant_id: str):
                 logger.debug("event_data={}".format(event_data))
         else:
             logger.debug("event_data={}".format(event_data))
+
+        participant_sets, paging_info = get_paging_info(participant_sets, paging)
 
         # TODO refactor to mapping
         participants = session.query(Participant).all()
@@ -142,4 +120,4 @@ def get_participant_sets(ranking_id: int, event_id: int, participant_id: str):
                 "set_datetime": ps.set_datetime,
             })
 
-    return ranking.__dict__, event_data, participant_data, sets
+    return ranking.__dict__, event_data, participant_data, sets, paging_info
