c16701f884694059fa804d408315dea814720bd0
rkaganda <rkaganda@gmail.com>
2022-04-03 10:02:02 -0700

    added page size

--- smash_gg_ranking/app.py
+++ smash_gg_ranking/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect
 from flask import render_template
 import logging
+from flask import request
 
 from config import config
 from views import ranking, events, participants, matches
@@ -12,6 +13,15 @@ logger.setLevel(config.settings['log_level'])
 handler = logging.FileHandler(filename=config.settings['log_path'], encoding='utf-8', mode='a')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
+
+
+def generate_paging():
+    paging = {
+        "page_num": request.args.get('page_num', default=1, type=int),
+        "page_size": request.args.get('page_size', default=config.settings['default_page_size'], type=int)
+    }
+    paging['page_size'] = min(config.settings['max_page_size'], paging['page_size'])
+    return paging
 
 
 @app.route('/')
@@ -33,8 +43,10 @@ def view_events(ranking_id=None):
 
 @app.route('/ranking/<ranking_id>/participants/')
 def view_ranking_participants(ranking_id=None):
-    ranking_data, participants_set = participants.get_ranking_participants(ranking_id)
-    return render_template('ranking/participants.html', participants=participants_set, ranking_data=ranking_data)
+    paging = generate_paging()
+    ranking_data, participants_set, paging_info = participants.get_ranking_participants(ranking_id, paging)
+    return render_template('ranking/participants.html', participants=participants_set, ranking_data=ranking_data,
+                           paging_info=paging_info)
 
 
 @app.route('/ranking/<ranking_id>/event/<event_id>/matches')
@@ -53,6 +65,8 @@ def view_ranking_participant_matches(ranking_id=None, participant_id=None):
 
 @app.route('/ranking/<ranking_id>/event/<event_id>/participant/<participant_id>/matches')
 def view_ranking_event_participant_matches(ranking_id=None, participant_id=None, event_id=None):
-    ranking_data, event_data, participant_data, participant_sets = matches.get_event_participant_sets(ranking_id, event_id, participant_id)
+    ranking_data, event_data, participant_data, participant_sets = matches.get_event_participant_sets(ranking_id,
+                                                                                                      event_id,
+                                                                                                      participant_id)
     return render_template('ranking/event/participant/matches.html', participant_sets=participant_sets,
                            participant_data=participant_data, ranking_data=ranking_data, event_data=event_data)
--- smash_gg_ranking/views/participants.py
+++ smash_gg_ranking/views/participants.py
@@ -1,5 +1,6 @@
 import logging
 from sqlalchemy import and_, or_
+from typing import Dict
 
 from db import db
 from db.models import Ranking, RankingEvent, ParticipantRanking, RankingSet, Participant
@@ -13,14 +14,25 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 
 
-def get_ranking_participants(ranking_id: int):
+def get_ranking_participants(ranking_id: int, paging: Dict):
     participants = []
     session = db.get_session()
     with session() as session:
         ranking = session.query(Ranking).where(Ranking.id == ranking_id).first()
+        participant_ranking_count = session.query(ParticipantRanking).where(
+            ParticipantRanking.ranking_id == ranking_id).count()
+        paging_info = {
+            "page_num": paging['page_num'],
+            "max_page": (participant_ranking_count // paging['page_size']) + 1,  # math is hard?
+            "page_size": paging['page_size']
+        }
+        offset = (paging['page_num']-1) * paging['page_size']
+        logger.debug(paging['page_size'])
+
         participant_ranking = session.query(ParticipantRanking).where(
-            ParticipantRanking.ranking_id == ranking_id).order_by(ParticipantRanking.participant_points.desc()).all()
-        rank = 1
+            ParticipantRanking.ranking_id == ranking_id).order_by(
+            ParticipantRanking.participant_points.desc()).offset(offset).limit(paging['page_size'])
+        rank = 1 + offset
 
         # TODO refactor to mapping
         participants_data = session.query(Participant).all()
@@ -44,4 +56,4 @@ def get_ranking_participants(ranking_id: int):
             })
             rank = rank + 1
 
-    return ranking, participants
+    return ranking, participants, paging_info
